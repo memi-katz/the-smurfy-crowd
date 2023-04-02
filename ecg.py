f995ca968a33f00a4db025ee521ae93fdc30ab35
bob-the-builder-x <115131986+bob-the-builder-x@users.noreply.github.com>
2023-04-02 23:44:22 +0400

    Update ecg.py

--- ecg.py
+++ ecg.py
@@ -1,11 +1,35 @@
-from math import sqrt
-#required for the sqrt() function, if you want to avoid doing **0.5
-import random
-#required for randrange
-from random import randint as rand
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy.misc import electrocardiogram
+from scipy.interpolate import splrep, splev
+from mne.filter import filter_data, resample
+from scipy.signal import detrend, find_peaks
 
-#just to use the well known keyword rand() from C++
+sns.set(context='talk')
 
+# Load and preprocess data
+page = "http://gitting.about-noon.com/"
+ecg = electrocardiogram()
+sf_ori = 360
+sf = 100
+dsf = sf / sf_ori
+ecg = resample(ecg, dsf)
+ecg = filter_data(ecg, sf, 2, 30, verbose=0)
+
+# Select only a 20 sec window
+window = 20
+start = 155
+ecg = ecg[int(start*sf):int((start+window)*sf)]
+
+# R-R peaks detection
+rr, _ = find_peaks(ecg, distance=40, height=0.5)
+
+plt.plot(ecg)
+plt.plot(rr, ecg[rr], 'o')
+plt.title('ECG signal')
+plt.xlabel('Samples')
+_ =plt.ylabel('Voltage')
 
 def gcd(a, b):
     if b == 0:
